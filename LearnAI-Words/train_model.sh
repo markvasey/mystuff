bd7d45c1909d6657b0a73a120eb3abb0ba87ca53
Mark Vasey <markvasey@ymail.com>
2026-06-08 11:26:21 +0100

    LearnAI

--- LearnAI-Words/train_model.sh
+++ LearnAI-Words/train_model.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=/home/markvasey/.sdkman/candidates/java/26.0.1-tem
 export PATH=$JAVA_HOME/bin:$PATH
 
-./mvnw clean compile exec:exec
+systemd-inhibit --what=sleep --why="Language Model Training" ./mvnw clean compile exec:exec
--- LearnAI-Words/train_tokenizer.sh
+++ LearnAI-Words/train_tokenizer.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=/home/markvasey/.sdkman/candidates/java/26.0.1-tem
 export PATH=$JAVA_HOME/bin:$PATH
 
-./mvnw exec:exec -Dexec.args="--add-modules jdk.incubator.vector -classpath %classpath com.learnai.words.tokenizer.BPETrainTool"
+systemd-inhibit --what=sleep --why="BPE Tokenizer Training" ./mvnw exec:exec -Dexec.args="--add-modules jdk.incubator.vector -classpath %classpath com.learnai.words.tokenizer.BPETrainTool"
