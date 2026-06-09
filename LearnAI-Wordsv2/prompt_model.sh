ee16469cdc7bbaf26a6936ab294ebfa72ec601b8
Mark Vasey <markvasey@ymail.com>
2026-06-09 20:15:56 +0100

    LearnAIv2

--- LearnAI-Wordsv2/prompt_model.sh
+++ LearnAI-Wordsv2/prompt_model.sh
@@ -2,4 +2,6 @@
 export JAVA_HOME=/home/markvasey/.sdkman/candidates/java/26.0.1-tem
 export PATH=$JAVA_HOME/bin:$PATH
 
-./mvnw exec:exec -Dexec.args="--add-modules jdk.incubator.vector -Dd.model=128 -Dblock.size=64 -classpath %classpath com.learnai.words.cli.PromptCLI"
+USE_GPU=${USE_GPU:-false}
+
+./mvnw exec:exec -Dexec.args="--add-modules jdk.incubator.vector --enable-native-access=ALL-UNNAMED -Duse.gpu=$USE_GPU -Dd.model=128 -Dblock.size=64 -classpath %classpath com.learnai.words.cli.PromptCLI"
--- LearnAI-Wordsv2/src/main/java/com/learnai/words/cli/PromptCLI.java
+++ LearnAI-Wordsv2/src/main/java/com/learnai/words/cli/PromptCLI.java
@@ -1,5 +1,6 @@
 package com.learnai.words.cli;
 
+import com.learnai.words.nn.GpuLanguageModel;
 import com.learnai.words.nn.LanguageModel;
 import com.learnai.words.nn.TextGenerator;
 import com.learnai.words.tokenizer.BPETokenizer;
@@ -32,13 +33,21 @@ public class PromptCLI {
 
         int dModel = Integer.getInteger("d.model", 128);
         int blockSize = Integer.getInteger("block.size", 64);
+        boolean useGpu = Boolean.getBoolean("use.gpu");
 
-        System.out.println("Loading language model (d_model=" + dModel + ", block_size=" + blockSize + ")...");
-        LanguageModel model = new LanguageModel(tokenizer.getVocabSize(), dModel, blockSize);
-        model.load(modelPath.toString());
-        System.out.println("Model loaded successfully!");
-
-        TextGenerator generator = new TextGenerator(model, tokenizer, blockSize);
+        final TextGenerator generator;
+        if (useGpu) {
+            System.out.println("Loading GPU language model (d_model=" + dModel + ", block_size=" + blockSize + ")...");
+            GpuLanguageModel model = new GpuLanguageModel(tokenizer.getVocabSize(), dModel, blockSize);
+            model.load(modelPath.toString());
+            generator = new TextGenerator(model, tokenizer, blockSize);
+        } else {
+            System.out.println("Loading CPU language model (d_model=" + dModel + ", block_size=" + blockSize + ")...");
+            LanguageModel model = new LanguageModel(tokenizer.getVocabSize(), dModel, blockSize);
+            model.load(modelPath.toString());
+            generator = new TextGenerator(model, tokenizer, blockSize);
+        }
+        
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
